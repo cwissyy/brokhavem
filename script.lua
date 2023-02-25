9db7073dc0c2d023dbe0997482893033e09f4000
cwissy lol <104460485+cwissyy@users.noreply.github.com>
2023-02-25 16:16:21 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("                                       Da Hood Script", "DarkTheme")
+local Window = Library.CreateLib("Brookhaven Script", "DarkTheme")
 
 
 -- Main
